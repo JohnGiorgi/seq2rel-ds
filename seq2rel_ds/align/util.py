f5050272aac4ebde2be1babba30dd51ac73a4050
johngiorgi <johnmgiorgi@gmail.com>
2021-04-21 10:34:24 -0400

    :sparkles: Use entity linking over fuzzy matching

--- seq2rel_ds/align/util.py
+++ seq2rel_ds/align/util.py
@@ -1,18 +1,13 @@
-import re
-from io import StringIO
-from typing import Dict, List, Optional, Union
+from typing import Optional, List
 
 import requests
 from requests.adapters import HTTPAdapter
 from seq2rel_ds.common.util import sanitize_text
 from urllib3.util.retry import Retry
 
+# API URLs
 UNIPROT_API_URL = "https://www.uniprot.org/uploadlists/"
 PUBTATOR_API_URL = "https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/pubtator"
-
-PROTEIN_NAME_REGEX = r"(?:(?:Full|Short)=)(?!\s)([^{;]*)(?<!\s)"
-GENE_NAME_REGEX = r"(?:Name=)(?!\s)([^{;]*)(?<!\s)"
-GENE_SYNONYMS_REGEX = r"(?:Synonyms=)(?!\s)([^{;]*)(?<!\s)"
 
 # Here, we create a session globally that can be used in all requests. We add a hook that will
 # call raise_for_status() after all our requests. Because API calls can be flaky, we also add
@@ -25,40 +20,12 @@ s.hooks["response"] = [assert_status_hook]
 retries = Retry(total=5, backoff_factor=0.1)
 s.mount("https://", HTTPAdapter(max_retries=retries))
 
-# Some custom types
-Synonyms = Dict[str, List[str]]
-Annotations = Dict[str, Dict[str, Dict[str, Union[str, List[str]]]]]
 
-
-def get_uniprot_synonyms(uniprot_ids: List[str]) -> Synonyms:
-    """Returns a dictionary, keyed by the ids in `uniprot_ids`, containing each ids gene and
-    protein name synonyms.
-    """
-    synonyms = {}
-    # See UniProts REST API for details: https://www.uniprot.org/help/api
-    file = StringIO(" ".join(uniprot_ids))
-    params = {"format": "txt", "from": "ACC+ID", "to": "ACC"}
-    r = s.post(UNIPROT_API_URL, files={"file": file}, params=params)
-    entries = r.text.strip().split("\n//")
-
-    for uniprot_id, entry in zip(uniprot_ids, entries):
-        synonyms[uniprot_id] = []
-        for line in entry.split("\n"):
-            if line.startswith("DE"):
-                synonyms[uniprot_id].extend(re.findall(PROTEIN_NAME_REGEX, line))
-            elif line.startswith("GN"):
-                synonyms[uniprot_id].extend(re.findall(GENE_NAME_REGEX, line))
-                gene_name_syns = re.findall(GENE_SYNONYMS_REGEX, line)
-                if gene_name_syns:
-                    synonyms[uniprot_id].extend(gene_name_syns[0].split(", "))
-    return synonyms
-
-
-def get_pubtator_response(pmids: List[str], concepts: Optional[List[str]] = None) -> Annotations:
-    json_body = {"pmids": pmids}
+def get_pubtator_response(pmids: List[str], concepts: Optional[List[str]] = None):
+    body = {"pmids": pmids}
     if concepts is not None:
-        json_body["concepts"] = concepts
-    r = s.post(PUBTATOR_API_URL, json=json_body)
+        body["concepts"] = concepts
+    r = s.post(PUBTATOR_API_URL, json=body)
     articles = r.text.strip().split("\n\n")
     annotations = {}
     for article in articles:
@@ -71,17 +38,34 @@ def get_pubtator_response(pmids: List[str], concepts: Optional[List[str]] = None
         title = sanitize_text(title)
         abstract = sanitize_text(abstract)
 
-        title_ents, abstract_ents = [], []
-        for ent in ents:
-            ent = ent.strip().split("\t")
-            start, text = ent[1], ent[3]
-            if int(start) < len(title):
-                title_ents.append(text)
-            else:
-                abstract_ents.append(text)
-
         annotations[pmid] = {
-            "title": {"text": title, "ents": title_ents},
-            "abstract": {"text": abstract, "ents": abstract_ents},
+            "title": {"text": title, "clusters": {}},
+            "abstract": {"text": abstract, "clusters": {}},
         }
+
+        for ent in ents:
+            ent_data = ent.strip().split("\t")
+            # A very small number of ents are missing IDs. Skip them.
+            if len(ent_data) < 6:
+                continue
+            _, start, end, text, _, entrezgene = ent_data
+            text = text.lower()
+            start, end = int(start), int(end)
+
+            # Collect entities per section
+            section = "title" if int(start) < len(title) else "abstract"
+
+            if entrezgene in annotations[pmid][section]["clusters"]:
+                # Don't retain duplicates
+                if text not in annotations[pmid][section]["clusters"][entrezgene]["ents"]:
+                    annotations[pmid][section]["clusters"][entrezgene]["ents"].append(text)
+                    annotations[pmid][section]["clusters"][entrezgene]["offsets"].append(
+                        (start, end)
+                    )
+            else:
+                annotations[pmid][section]["clusters"][entrezgene] = {
+                    "ents": [text],
+                    "offsets": [(start, end)],
+                }
+
     return annotations
