2c65703a8997f9c7c04a47d59ea2d72801ad24aa
johngiorgi <johnmgiorgi@gmail.com>
2021-04-21 10:34:49 -0400

    :white_check_mark: Update tests

--- tests/align/test_align_util.py
+++ tests/align/test_align_util.py
@@ -1,93 +1,6 @@
 from seq2rel_ds.align import util
 
 
-def test_get_uniprot_synonyms_one_id() -> None:
-    """Test that the function works for a single Uniprot ID component names."""
-    expected = {
-        "Q09670": ["Putative NADPH dehydrogenase C5H10.04", "Old yellow enzyme homolog 1"],
-    }
-
-    actual = util.get_uniprot_synonyms(uniprot_ids=list(expected.keys()))
-    assert actual == expected
-
-
-def test_get_uniprot_synonyms_gene_names() -> None:
-    """Test that the function captures gene names."""
-    expected = {
-        "Q86V15": [
-            "Zinc finger protein castor homolog 1",
-            "Castor-related protein",
-            "Putative survival-related protein",
-            "Zinc finger protein 693",
-            "CASZ1",
-            "CST",
-            "SRG",
-            "ZNF693",
-        ],
-    }
-
-    actual = util.get_uniprot_synonyms(uniprot_ids=list(expected.keys()))
-    assert actual == expected
-
-
-def test_get_uniprot_synonyms_ignores_ids() -> None:
-    """Test that any IDs provided as part of the name are ignored."""
-    expected = {
-        "Q20819": ["Elongation factor Ts, mitochondrial", "EF-Ts", "EF-TsMt", "tsfm-1"],
-    }
-    actual = util.get_uniprot_synonyms(uniprot_ids=list(expected.keys()))
-    assert actual == expected
-
-
-def test_get_uniprot_synonyms_component_names() -> None:
-    """Test that the function captures component names."""
-    expected = {
-        "P33587": [
-            "Vitamin K-dependent protein C",
-            "Anticoagulant protein C",
-            "Autoprothrombin IIA",
-            "Blood coagulation factor XIV",
-            "Vitamin K-dependent protein C light chain",
-            "Vitamin K-dependent protein C heavy chain",
-            "Activation peptide",
-            "Proc",
-        ],
-    }
-
-    actual = util.get_uniprot_synonyms(uniprot_ids=list(expected.keys()))
-    assert actual == expected
-
-
-def test_get_uniprot_synonyms_many_ids() -> None:
-    expected = {
-        "Q09670": ["Putative NADPH dehydrogenase C5H10.04", "Old yellow enzyme homolog 1"],
-        "Q86V15": [
-            "Zinc finger protein castor homolog 1",
-            "Castor-related protein",
-            "Putative survival-related protein",
-            "Zinc finger protein 693",
-            "CASZ1",
-            "CST",
-            "SRG",
-            "ZNF693",
-        ],
-        "Q20819": ["Elongation factor Ts, mitochondrial", "EF-Ts", "EF-TsMt", "tsfm-1"],
-        "P33587": [
-            "Vitamin K-dependent protein C",
-            "Anticoagulant protein C",
-            "Autoprothrombin IIA",
-            "Blood coagulation factor XIV",
-            "Vitamin K-dependent protein C light chain",
-            "Vitamin K-dependent protein C heavy chain",
-            "Activation peptide",
-            "Proc",
-        ],
-    }
-
-    actual = util.get_uniprot_synonyms(uniprot_ids=list(expected.keys()))
-    assert actual == expected
-
-
 def test_get_pubtator_response() -> None:
     pmid = "28483577"
     expected = {
@@ -98,7 +11,7 @@ def test_get_pubtator_response() -> None:
                     " deacetylation and anti-inflammatory activities in bronchial epithelial cells"
                     " exposed to cigarette smoke."
                 ),
-                "ents": [],
+                "clusters": {},
             },
             "abstract": {
                 "text": (
@@ -125,17 +38,14 @@ def test_get_pubtator_response() -> None:
                     " some processes related to steroid resistance induced by oxidative stress due to cigarette"
                     " smoke exposure increasing the anti-inflammatory effects of FP."
                 ),
-                "ents": [
-                    "HDAC3",
-                    "HDAC2",
-                    "NF-kappaB",
-                    "IL-8",
-                    "TNF-alpha",
-                    "IL-1beta",
-                    "HDAC3",
-                    "HDAC2",
-                    "NF-kappaB",
-                ],
+                "clusters": {
+                    "8841": {"ents": ["HDAC3"], "offsets": [(921, 926)]},
+                    "3066": {"ents": ["HDAC2"], "offsets": [(931, 936)]},
+                    "4790": {"ents": ["NF-kappaB"], "offsets": [(979, 988)]},
+                    "3576": {"ents": ["IL-8"], "offsets": [(1013, 1017)]},
+                    "7124": {"ents": ["TNF-alpha"], "offsets": [(1019, 1028)]},
+                    "3553": {"ents": ["IL-1beta"], "offsets": [(1030, 1038)]},
+                },
             },
         }
     }
