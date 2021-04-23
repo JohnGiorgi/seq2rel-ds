f93930381c1b94a98dee761ccacec07b81e8d756
johngiorgi <johnmgiorgi@gmail.com>
2021-04-23 13:39:25 -0400

    :recycle: Split query_pubtator into two functions

--- seq2rel_ds/align/util.py
+++ seq2rel_ds/align/util.py
@@ -1,8 +1,10 @@
-from typing import Optional, List
+from typing import Dict, List, Optional
 
 import requests
 from requests.adapters import HTTPAdapter
-from seq2rel_ds.common.util import sanitize_text
+from seq2rel_ds.common import util
+from seq2rel_ds.common.schemas import PubtatorAnnotation
+from seq2rel_ds.common.util import TextSegment
 from urllib3.util.retry import Retry
 
 # API URLs
@@ -20,51 +22,18 @@ retries = Retry(total=5, backoff_factor=0.1)
 s.mount("https://", HTTPAdapter(max_retries=retries))
 
 
-def get_pubtator_response(pmids: List[str], concepts: Optional[List[str]] = None):
+def query_pubtator(
+    pmids: List[str],
+    concepts: Optional[List[str]] = None,
+    text_segment: TextSegment = TextSegment.both,
+    skip_malformed: bool = False,
+) -> Dict[str, PubtatorAnnotation]:
     body = {"pmids": pmids}
     if concepts is not None:
         body["concepts"] = concepts
     r = s.post(PUBTATOR_API_URL, json=body)
-    articles = r.text.strip().split("\n\n")
-    annotations = {}
-    for article in articles:
-        article = article.strip().split("\n")
-        title, abstract, ents = article[0], article[1], article[2:]
-        pmid, title = title.split("|t|")
-        abstract = abstract.split("|a|")[-1]
-
-        # Some very basic preprocessing
-        title = sanitize_text(title)
-        abstract = sanitize_text(abstract)
-
-        annotations[pmid] = {
-            "title": {"text": title, "clusters": {}},
-            "abstract": {"text": abstract, "clusters": {}},
-        }
-
-        for ent in ents:
-            ent_data = ent.strip().split("\t")
-            # A very small number of ents are missing IDs. Skip them.
-            if len(ent_data) < 6:
-                continue
-            _, start, end, text, _, entrezgene = ent_data
-            text = text.lower()
-            start, end = int(start), int(end)
-
-            # Collect entities per section
-            section = "title" if int(start) < len(title) else "abstract"
-
-            if entrezgene in annotations[pmid][section]["clusters"]:
-                # Don't retain duplicates
-                if text not in annotations[pmid][section]["clusters"][entrezgene]["ents"]:
-                    annotations[pmid][section]["clusters"][entrezgene]["ents"].append(text)
-                    annotations[pmid][section]["clusters"][entrezgene]["offsets"].append(
-                        (start, end)
-                    )
-            else:
-                annotations[pmid][section]["clusters"][entrezgene] = {
-                    "ents": [text],
-                    "offsets": [(start, end)],
-                }
-
+    pubtator_content = r.text.strip()
+    annotations = util.parse_pubtator(
+        pubtator_content, text_segment=text_segment, skip_malformed=skip_malformed
+    )
     return annotations
