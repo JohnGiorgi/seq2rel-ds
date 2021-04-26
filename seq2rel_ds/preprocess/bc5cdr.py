06e62ea31304ca026d1e3a7a8b80152db9e7fadd
johngiorgi <johnmgiorgi@gmail.com>
2021-04-26 15:46:08 -0400

    :recycle: Cleanup BC5CDR script

--- seq2rel_ds/preprocess/bc5cdr.py
+++ seq2rel_ds/preprocess/bc5cdr.py
@@ -1,145 +1,71 @@
-from operator import itemgetter
 from pathlib import Path
-from typing import Any, Dict, List, Tuple
+from typing import List
 
 import typer
-from seq2rel_ds.common.util import set_seeds
+from seq2rel_ds.common import util
 
-app = typer.Typer(callback=set_seeds)
+app = typer.Typer()
 
 TRAIN_FILENAME = "CDR_TrainingSet.PubTator.txt"
 VALID_FILENAME = "CDR_DevelopmentSet.PubTator.txt"
 TEST_FILENAME = "CDR_TestSet.PubTator.txt"
 
 
-def format_entity(ent_text: str, ent_type: str):
-    return f"{ent_text} <{ent_type.upper()}>"
+def _preprocess_bc5cdr(filepath: Path) -> List[str]:
+    pubtator_content = Path(filepath).read_text()
+    parsed = util.parse_pubtator(
+        pubtator_content=pubtator_content, text_segment=util.TextSegment.both
+    )
 
-
-def format_relation(ent_1_text, ent_1_type, ent_2_text, ent_2_type, rel_type):
-    ent_1 = format_entity(ent_1_text, ent_1_type)
-    ent_2 = format_entity(ent_2_text, ent_2_type)
-    return f"<{rel_type.upper()}> {ent_1} {ent_2} </{rel_type.upper()}>"
-
-
-def parse_entity(annotation: List[str]) -> List[Tuple[str, str, str, str, str]]:
-    """Returns each entity in `annotation` as a list of tuples containing the end index, entity
-    text, entity type, and entity ID.
-    """
-    entities = []
-    # This is a simple entity that may have one or more IDs.
-    if len(annotation) == 6:
-        _, ent_start, _, ent_texts, ent_type, ent_ids = annotation
-        ent_ids = ent_ids.split("|")
-        ent_texts = [ent_texts] * len(ent_ids)
-    # This is a nested entity with multiple IDs.
-    else:
-        _, ent_start, _, _, ent_type, ent_ids, ent_texts = annotation
-        ent_ids = ent_ids.split("|")
-        ent_texts = ent_texts.split("|")
-
-    offset = int(ent_start)
-    for ent_id, ent_text in zip(ent_ids, ent_texts):
-        ent_start, ent_end = offset, offset + len(ent_text)
-        entities.append((ent_start, ent_end, ent_text, ent_type, ent_id))
-
-    return entities
-
-
-def parse_relation(annotation: str, entities: Dict[str, Any]):
-    """Formats the relation contained in `annotation`, resolving the IDs using `entities`."""
-    _, rel_type, ent_1_id, ent_2_id = annotation
-    (
-        _,
-        ent_1_end,
-        ent_1_text,
-        ent_1_type,
-    ) = entities[ent_1_id]
-    _, ent_2_end, ent_2_text, ent_2_type = entities[ent_2_id]
-
-    relation = format_relation(ent_1_text, ent_1_type, ent_2_text, ent_2_type, rel_type)
-    offset = ent_1_end + ent_2_end
-    to_pop = [ent_1_id, ent_2_id]
-
-    return relation, offset, to_pop
-
-
-def unpack_chunk(chunk: List[str]):
-    lines = chunk.split("\n")
-
-    title = lines[0].split("|")[-1]
-    abstract = lines[1].split("|")[-1]
-
-    entities = {}
-    relations, offsets, ent_ids_to_pop = [], [], []
-
-    for line in lines[2:]:
-        if not line:
-            continue
-        annotation = line.split("\t")
-        if len(annotation) > 4:  # this is an entity
-            for entity in parse_entity(annotation):
-                # Some entities don't have unique identifiers. Retain all of them.
-                ent_start, ent_end, ent_text, ent_type, ent_id = entity
-                if ent_id == "-1":
-                    entities[ent_text] = (ent_start, ent_end, ent_text, ent_type)
-                # Otherwise retain the first mention of each entity.
-                elif ent_id not in entities:
-                    entities[ent_id] = (ent_start, ent_end, ent_text, ent_type)
-        else:  # this is a relation
-            relation, offset, to_pop = parse_relation(annotation, entities)
-            # Don't retain relations that are otherwise identical except for entity offsets or case.
-            if relation.lower() not in map(str.lower, relations):
-                relations.append(relation)
-                offsets.append(offset)
-                ent_ids_to_pop.extend(to_pop)
-
-    # Remove entities already accounted for by relation annotations.
-    for ent_id in ent_ids_to_pop:
-        entities.pop(ent_id, None)
-
-    entities = [format_entity(*entity[2:]) for entity in entities.values()]
-
-    return title, abstract, entities, relations, offsets
-
-
-def preprocess_bc5cdr(filepath: str) -> List[str]:
-
-    # Step 1: Process the dataset chunk by chunk, collecting formatted relations and their offsets.
     processed_dataset = []
-    chunks = Path(filepath).read_text().split("\n\n")
-    with typer.progressbar(chunks, label="Processing") as progress:
-        for chunk in progress:
-            if not chunk:
-                continue
-            title, abstract, entities, relations, offsets = unpack_chunk(chunk)
 
-            # Pack, sort, and unpack relations and their offsets
-            relations = list(zip(*[relations, offsets]))
-            relations.sort(key=itemgetter(1))
-            relations, _ = list(zip(*relations))
+    for annotation in parsed.values():
+        relations = []
+        offsets = []
 
-            # Take the text to be the abstract and the title.
-            text = f"{title} {abstract}"
-            # Take the annotation to be the entities and the relations
-            annotation = " ".join(entities + list(relations))
+        for rel in annotation.relations:
+            uid_1, uid_2, rel_label = rel
+            # Keep track of the end offsets of each entity. We will use these to sort
+            # relations according to their order of first appearence in the text.
+            offset_1 = min((end for _, end in annotation.clusters[uid_1].offsets))
+            offset_2 = min((end for _, end in annotation.clusters[uid_2].offsets))
+            offset = offset_1 + offset_2
+            ent_clusters = [annotation.clusters[uid_1].ents, annotation.clusters[uid_2].ents]
+            ent_labels = [annotation.clusters[uid_1].label, annotation.clusters[uid_2].label]
+            relation = util.format_relation(
+                ent_clusters=ent_clusters,
+                ent_labels=ent_labels,
+                rel_label=rel_label,
+            )
+            relations.append(relation)
+            offsets.append(offset)
 
-            processed_dataset.append(f"{text}\t{annotation}")
+        relations = util.sort_by_offset(relations, offsets)
+        processed_dataset.append(f"{annotation.text}\t{' '.join(relations)}")
 
     return processed_dataset
 
 
-@app.command(name="bc5cdr")
-def main(input_dir: Path, output_dir: str) -> None:
+@app.callback(invoke_without_command=True)
+def main(
+    input_dir: Path = typer.Argument(..., help="Path to a local copy of the BC5CDR corpus."),
+    output_dir: Path = typer.Argument(..., help="Directory path to save the preprocessed data."),
+) -> None:
+    """Given a path to a local copy of the BC5CDR corpus (`input_dir`), saves a preprocessed copy
+    of the data to `output_dir` that can be used to train a model with the seq2rel package.
+
+    See https://github.com/JohnGiorgi/seq2rel for more details on the seq2rel package and
+    https://pubmed.ncbi.nlm.nih.gov/27161011/ for more details on the BC5CDR corpus.
+    """
     train_filepath = Path(input_dir) / TRAIN_FILENAME
     dev_filepath = Path(input_dir) / VALID_FILENAME
     test_filepath = Path(input_dir) / TEST_FILENAME
 
-    train = preprocess_bc5cdr(train_filepath)
-    valid = preprocess_bc5cdr(dev_filepath)
-    test = preprocess_bc5cdr(test_filepath)
+    train = _preprocess_bc5cdr(train_filepath)
+    valid = _preprocess_bc5cdr(dev_filepath)
+    test = _preprocess_bc5cdr(test_filepath)
 
-    output_dir: Path = Path(output_dir)
+    output_dir = Path(output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
 
     (output_dir / "train.tsv").write_text("\n".join(train))
