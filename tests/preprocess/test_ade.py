9f52355658191e9a75b1edee2d6036015eba2819
johngiorgi <johnmgiorgi@gmail.com>
2021-04-27 14:23:59 -0400

    :white_check_mark: Update tests

---
+++ tests/preprocess/test_ade.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+from seq2rel_ds.common.testing import Seq2RelDSTestCase
+from seq2rel_ds.preprocess import ade
+from typer.testing import CliRunner
+from seq2rel_ds.common import util
+
+runner = CliRunner()
+
+
+class TestADE(Seq2RelDSTestCase):
+    def test_preprocess_ade(self) -> None:
+        # In absense of testing the entire data set, we sanity check the first and last examples
+        actual = ade._preprocess_ade_v2()
+        assert actual[0] == (
+            "Intravenous azithromycin-induced ototoxicity."
+            f"\t@{ade.REL_LABEL}@ azithromycin @{ade.DRUG_LABEL}@ ototoxicity @{ade.EFFECT_LABEL}@ {util.END_OF_REL_SYMBOL}"
+        )
+        assert actual[-1] == (
+            "Successful challenge with clozapine in a history of eosinophilia."
+            f"\t@{ade.REL_LABEL}@ clozapine @{ade.DRUG_LABEL}@ eosinophilia @{ade.EFFECT_LABEL}@ {util.END_OF_REL_SYMBOL}"
+        )
+
+    def test_ade_command(self, tmp_path: Path) -> None:
+        output_dir = str(tmp_path)
+        result = runner.invoke(ade.app, [output_dir])
+        assert result.exit_code == 0
+
+        # Check that the expected files were created
+        assert (tmp_path / "train.tsv").is_file()
+        assert (tmp_path / "valid.tsv").is_file()
+        assert (tmp_path / "test.tsv").is_file()
