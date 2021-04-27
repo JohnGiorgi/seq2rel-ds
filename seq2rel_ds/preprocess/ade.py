66390d09d5c644957d54a8cf48fa8601e50850f3
johngiorgi <johnmgiorgi@gmail.com>
2021-04-27 14:23:33 -0400

    :recycle: Cleanup ADE script

--- seq2rel_ds/preprocess/ade.py
+++ seq2rel_ds/preprocess/ade.py
@@ -1,22 +1,16 @@
-from operator import itemgetter
 from pathlib import Path
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Any, Dict, List, Tuple
 
 import typer
 from datasets import load_dataset
-from seq2rel_ds.common.util import train_valid_test_split
-
-DatasetLine = Dict[str, Any]
+from seq2rel_ds.common import util
 
 app = typer.Typer()
 
 
-def format_adverse_drug_reaction(drug: str, effect: str) -> str:
-    return f"@ADE@ {drug} @DRUG@ {effect} @EFFECT@ @EOR@"
-
-
-def unpack_line(line: DatasetLine) -> Tuple[str, str, str, str]:
-    return line["text"], line["drug"], line["effect"], line["indexes"]
+REL_LABEL = "ADE"
+DRUG_LABEL = "DRUG"
+EFFECT_LABEL = "EFFECT"
 
 
 def get_offsets(text: str, drug: str, effect: str, indexes: Dict[str, Any]) -> Tuple[int]:
@@ -40,17 +34,26 @@ def get_offsets(text: str, drug: str, effect: str, indexes: Dict[str, Any]) -> T
     return drug_start, drug_end, effect_start, effect_end
 
 
-def preprocess_ade_v2() -> List[str]:
+def _preprocess_ade_v2() -> List[str]:
     dataset = load_dataset("ade_corpus_v2", "Ade_corpus_v2_drug_ade_relation")["train"]
 
     # Step 1: Process the dataset line by line, collecting formatted relations and their offsets.
     processed_dataset = {}
     with typer.progressbar(dataset, label="Processing") as progress:
         for line in progress:
-            text, drug, effect, indexes = unpack_line(line)
-            relation = format_adverse_drug_reaction(drug, effect)
+            text = line["text"]
+            drug = line["drug"]
+            effect = line["effect"]
+            indexes = line["indexes"]
+
+            relation = util.format_relation(
+                ent_clusters=[[drug]] + [[effect]],
+                ent_labels=[DRUG_LABEL, EFFECT_LABEL],
+                rel_label=REL_LABEL,
+            )
             # We take the relations position to be the sum of end indices of its entities.
-            offset = sum(get_offsets(text, drug, effect, indexes)[1::2])
+            _, drug_end, _, effect_end = get_offsets(text, drug, effect, indexes)
+            offset = drug_end + effect_end
 
             # Don't retain relations that are otherwise identical except for entity offsets or case.
             if text in processed_dataset:
@@ -60,23 +63,20 @@ def preprocess_ade_v2() -> List[str]:
             else:
                 processed_dataset[text] = {"relations": [relation], "offsets": [offset]}
 
-    # Step 2: Potentially sort the inputs
-    processed_examples = []
+    # Step 2: Sort the relations
+    sorted_examples = []
     for text, example in processed_dataset.items():
-        # Pack, sort, and unpack relations and their offsets
-        example = list(zip(*example.values()))
-        example.sort(key=itemgetter(1))
-        relations, _ = list(zip(*example))
-        processed_examples.append(f"{text}\t{' '.join(relations)}")
+        relations = util.sort_by_offset(example["relations"], example["offsets"])
+        sorted_examples.append(f"{text}\t{' '.join(relations)}")
 
-    return processed_examples
+    return sorted_examples
 
 
 @app.callback(invoke_without_command=True)
-def main(output_dir: Path, sorting: Optional[str] = None) -> None:
+def main(output_dir: Path) -> None:
     """Download and preprocess the ADE V2 corpus for use with seq2rel."""
-    dataset = preprocess_ade_v2()
-    train, valid, test = train_valid_test_split(dataset)
+    dataset = _preprocess_ade_v2()
+    train, valid, test = util.train_valid_test_split(dataset)
 
     output_dir: Path = Path(output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
