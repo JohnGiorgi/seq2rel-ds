e720dbb392afeeb9397c9ddd56ed1cb71edcff98
johngiorgi <johnmgiorgi@gmail.com>
2021-05-13 16:29:37 -0400

    :sparkles: Add entity hinting option

--- seq2rel_ds/preprocess/bc5cdr.py
+++ seq2rel_ds/preprocess/bc5cdr.py
@@ -17,18 +17,21 @@ VALID_FILENAME = "CDR_DevelopmentSet.PubTator.txt"
 TEST_FILENAME = "CDR_TestSet.PubTator.txt"
 
 
-def _preprocess(pubtator_content: str) -> List[str]:
+def _preprocess(pubtator_content: str, include_ent_hints: bool = False) -> List[str]:
     pubtator_annotations = util.parse_pubtator(
         pubtator_content=pubtator_content, text_segment=util.TextSegment.both, sort_ents=True
     )
-    seq2rel_annotations = util.pubtator_to_seq2rel(pubtator_annotations)
+    seq2rel_annotations = util.pubtator_to_seq2rel(pubtator_annotations, include_ent_hints)
 
     return seq2rel_annotations
 
 
-@app.callback(invoke_without_command=True)
+@app.command()
 def main(
     output_dir: Path = typer.Argument(..., help="Directory path to save the preprocessed data."),
+    include_ent_hints: bool = typer.Option(
+        False, help="Include entity location and type hints in the text"
+    ),
 ) -> None:
     """Preprocess a local copy of the BC5CDR corpus for use with seq2rel."""
     msg.divider("Preprocessing BC5CDR")
@@ -41,17 +44,17 @@ def main(
 
     with msg.loading("Preprocessing the training data..."):
         raw = z.read(str(Path(PARENT_DIR) / TRAIN_FILENAME)).decode("utf8")
-        train = _preprocess(raw)
+        train = _preprocess(raw, include_ent_hints)
     msg.good("Preprocessed the training data")
 
     with msg.loading("Preprocessing the validation data..."):
         raw = z.read(str(Path(PARENT_DIR) / VALID_FILENAME)).decode("utf8")
-        valid = _preprocess(raw)
+        valid = _preprocess(raw, include_ent_hints)
     msg.good("Preprocessed the validation data")
 
     with msg.loading("Preprocessing the test data..."):
         raw = z.read(str(Path(PARENT_DIR) / TEST_FILENAME)).decode("utf8")
-        test = _preprocess(raw)
+        test = _preprocess(raw, include_ent_hints)
     msg.good("Preprocessed the test data")
 
     output_dir = Path(output_dir)
