d982a399c7345d23e8edb5dd2972b222662993a0
johngiorgi <johnmgiorgi@gmail.com>
2021-11-28 11:45:43 -0500

    :bug: Add missing dgm app to main

--- seq2rel_ds/preprocess/main.py
+++ seq2rel_ds/preprocess/main.py
@@ -1,14 +1,14 @@
 import typer
 from seq2rel_ds.common.util import set_seeds
-from seq2rel_ds.preprocess import ade, bc5cdr, docred, gda
+from seq2rel_ds.preprocess import bc5cdr, dgm, docred, gda
 
 set_seeds()
 
 app = typer.Typer(
     help="Commands for preprocessing data to conform to the seq2rel format.",
 )
-app.add_typer(ade.app, name="ade")
 app.add_typer(bc5cdr.app, name="bc5cdr")
+app.add_typer(dgm.app, name="dgm")
 app.add_typer(docred.app, name="docred")
 app.add_typer(gda.app, name="gda")
 
