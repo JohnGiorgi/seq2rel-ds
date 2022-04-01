2d728341d4a518f22831b4fd1916294759cea89b
John Giorgi <johnmgiorgi@gmail.com>
2022-04-01 11:52:28 -0400

    :fire: Remove align module

--- seq2rel_ds/main.py
+++ seq2rel_ds/main.py
@@ -1,13 +1,17 @@
 import typer
-import seq2rel_ds.align.main as align
-import seq2rel_ds.preprocess.main as preprocess
 from seq2rel_ds.common.util import set_seeds
+from seq2rel_ds import cdr, dgm, docred, gda
 
 set_seeds()
 
-app = typer.Typer()
-app.add_typer(align.app, name="align")
-app.add_typer(preprocess.app, name="preprocess")
+app = typer.Typer(
+    help="Commands for preprocessing data to conform to the seq2rel format.",
+)
+app.add_typer(cdr.app, name="cdr")
+app.add_typer(dgm.app, name="dgm")
+app.add_typer(docred.app, name="docred")
+app.add_typer(gda.app, name="gda")
+
 
 if __name__ == "__main__":
     app()
