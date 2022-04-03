02ce39b6450303f2842af0b992de49d4b91abd80
John Giorgi <johnmgiorgi@gmail.com>
2022-04-03 15:54:47 -0400

    :bug: Better character offsets for DGM

--- seq2rel_ds/common/text_utils.py
+++ seq2rel_ds/common/text_utils.py
@@ -1,5 +1,17 @@
 # Public functions #
 
+from typing import Iterable
+
+
+def findall(string: str, substring: str) -> Iterable[int]:
+    """Find all overlapping occurrences of `substring` in a `string`.
+    Taken from: https://stackoverflow.com/a/34445090/6578628
+    """
+    i = string.find(substring)
+    while i != -1:
+        yield i
+        i = string.find(substring, i + 1)
+
 
 def sanitize_text(text: str, lowercase: bool = False) -> str:
     """Cleans text by removing whitespace, newlines and tabs and (optionally) lowercasing."""
